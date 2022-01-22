f8145ea05a93fdb6ed7e1a494242652e07c2a9e5
ZIDAN <zidanali111@gmail.com>
2022-01-22 20:54:56 +0530

    Users Model Committed.

--- app/src/main/java/com/example/chat/SignUpActivity.java
+++ app/src/main/java/com/example/chat/SignUpActivity.java
@@ -1,11 +1,16 @@
 package com.example.chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.chat.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -33,7 +38,19 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View view) {
                 auth.createUserWithEmailAndPassword
                         (binding.etEmail.getText().toString(),
-                        binding.etPassword.getText().toString());
+                        binding.etPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this,"User Created Successfully",Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            Toast.makeText(SignUpActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
 
             }
