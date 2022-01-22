8c9c3a4fb0e475cfbbc9f62a1380c70492463b2e
ZIDAN <zidanali111@gmail.com>
2022-01-22 21:43:25 +0530

    SignIn and SignUp Backend Using Firebase committed.

--- app/src/main/java/com/example/chat/SignUpActivity.java
+++ app/src/main/java/com/example/chat/SignUpActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -79,6 +80,14 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+        binding.tvAlreadyAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SignUpActivity.this,SignInActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
