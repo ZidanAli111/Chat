00ef0b627f651660fe0ee799d4779d88eee12a59
ZIDAN <zidanali111@gmail.com>
2022-01-22 20:39:14 +0530

    Firebase SignUp with email and Password committed.

--- app/src/main/java/com/example/chat/SignUpActivity.java
+++ app/src/main/java/com/example/chat/SignUpActivity.java
@@ -3,19 +3,42 @@ package com.example.chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.chat.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
     ActivitySignUpBinding binding;
+
+    private FirebaseAuth auth;
+
+    FirebaseDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
         getSupportActionBar().hide();
+
+        auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.createUserWithEmailAndPassword
+                        (binding.etEmail.getText().toString(),
+                        binding.etPassword.getText().toString());
+
+
+            }
+        });
+
+
     }
 }
