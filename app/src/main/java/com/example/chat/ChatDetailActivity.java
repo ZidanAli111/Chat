6bb6a64615b2387fb36ba4203420ba8622ffd0c5
ZIDAN <zidanali111@gmail.com>
2022-01-23 19:32:59 +0530

    Chat Layout Design committed.

---
+++ app/src/main/java/com/example/chat/ChatDetailActivity.java
@@ -0,0 +1,22 @@
+package com.example.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chat.databinding.ActivityChatDetailBinding;
+
+public class ChatDetailActivity extends AppCompatActivity {
+
+    ActivityChatDetailBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityChatDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
