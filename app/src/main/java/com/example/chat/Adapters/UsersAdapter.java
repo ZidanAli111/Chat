bfc42c3897800592ab78a515a2468211aa62f32b
ZIDAN <zidanali111@gmail.com>
2022-01-24 22:28:57 +0530

    show last message in user list committed.

--- app/src/main/java/com/example/chat/Adapters/UsersAdapter.java
+++ app/src/main/java/com/example/chat/Adapters/UsersAdapter.java
@@ -14,6 +14,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.chat.ChatDetailActivity;
 import com.example.chat.Models.Users;
 import com.example.chat.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -49,6 +54,29 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> 
                 .into(holder.image);
 
         holder.userName.setText(users.getUserName());
+
+        FirebaseDatabase.getInstance().getReference().child("chats")
+                .child(FirebaseAuth.getInstance().getUid()+users.getUserId())
+                .orderByChild("timestamp")
+                .limitToLast(1)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.hasChildren())
+                        {
+                            for (DataSnapshot snapshot1:snapshot.getChildren())
+                            {
+                                holder.lastMessage.setText(snapshot1.child("message").getValue().toString());
+
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
