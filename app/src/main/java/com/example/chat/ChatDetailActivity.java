6f9a6ca8cd1db16c0e8acc6806f4bc658b268791
ZIDAN <zidanali111@gmail.com>
2022-01-24 19:08:13 +0530

    show message in chat RecyclerView committed.

--- app/src/main/java/com/example/chat/ChatDetailActivity.java
+++ app/src/main/java/com/example/chat/ChatDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -12,7 +13,10 @@ import com.example.chat.Models.MessageModel;
 import com.example.chat.databinding.ActivityChatDetailBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -35,7 +39,7 @@ public class ChatDetailActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
 
-     final    String senderId = auth.getUid();
+        final String senderId = auth.getUid();
         String receivedId = getIntent().getStringExtra("userId");
         String userName = getIntent().getStringExtra("userName");
         String profilePic = getIntent().getStringExtra("profilePic");
@@ -47,50 +51,74 @@ public class ChatDetailActivity extends AppCompatActivity {
         binding.backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(ChatDetailActivity.this,MainActivity.class);
+                Intent intent = new Intent(ChatDetailActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
 
-        final ArrayList<MessageModel> messageModels=new ArrayList<>();
-        final ChatAdapter chatAdapter=new ChatAdapter(messageModels,this);
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+        final ChatAdapter chatAdapter = new ChatAdapter(messageModels, this);
         binding.chatRecyclerView.setAdapter(chatAdapter);
 
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         binding.chatRecyclerView.setLayoutManager(layoutManager);
 
-        final String senderRoom=senderId+receivedId;
-        final  String receiverRoom=receivedId+senderId;
+        final String senderRoom = senderId + receivedId;
+        final String receiverRoom = receivedId + senderId;
+
+
+        database.getReference().child("chats")
+                .child(senderRoom)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        messageModels.clear();
+
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+
+                            MessageModel model = snapshot1.getValue(MessageModel.class);
+                            messageModels.add(model);
+                        }
+
+                        chatAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
 
         binding.send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-              String message=  binding.etMessage.getText().toString();
+                String message = binding.etMessage.getText().toString();
 
-              final MessageModel model=new MessageModel(senderId,message);
-              model.setTimestamp(new Date().getTime());
-              binding.etMessage.setText("");
+                final MessageModel model = new MessageModel(senderId, message);
+                model.setTimestamp(new Date().getTime());
+                binding.etMessage.setText("");
 
 
-              database.getReference().child("chats")
-                      .child(senderRoom)
-                      .push()
-                      .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
-                  @Override
-                  public void onSuccess(Void unused) {
-                      database.getReference().child("chats")
-                              .child(receiverRoom)
-                              .push()
-                              .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
-                          @Override
-                          public void onSuccess(Void unused) {
+                database.getReference().child("chats")
+                        .child(senderRoom)
+                        .push()
+                        .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        database.getReference().child("chats")
+                                .child(receiverRoom)
+                                .push()
+                                .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
 
-                          }
-                      });
-                  }
-              });
+                            }
+                        });
+                    }
+                });
 
             }
         });
